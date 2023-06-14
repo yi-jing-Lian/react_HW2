a3c039798b4c2b278405a0a87f502a49adaaadb5
Lian <a0976008161@gmail.com>
2023-06-14 19:34:32 +0800

    antd怪怪的

--- src/components/Page4.js
+++ src/components/Page4.js
@@ -1,14 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import doge from "../images/doge.jpg";
+import { Space, Table, Tag } from 'antd';
 
+
+
+const columns = [
+    {
+      title: 'id',
+      dataIndex: 'id',
+      sorter: {
+        compare: (a, b) => a.id - b.id,
+        multiple: 3,
+      },
+    },
+    {
+      title: '圖片網址',
+      dataIndex: 'pictureUrl',
+    },
+    {
+      title: 'NFT名稱',
+      dataIndex: 'nftName',
+    },
+    {
+      title: 'qrcode',
+      dataIndex: 'qrcode',
+    },
+  ];
+
+  const onChange = (pagination, filters, sorter, extra) => {
+    console.log('params', pagination, filters, sorter, extra);
+  };
 function Page4(){
-    return (        
+  // const [image,setImage]=useState();
+  // useEffect(()=>{
+  //   fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then((data) => data.json())
+  //     .then((data)=>{
+  //       console.log(data.message);
+  //       setImage(data.message);
+  //     });
+  //   },[]);
+    const [task, setTask]=useState('');
+    useEffect(()=>{
+      fetch("http://ec2-35-77-78-80.ap-northeast-1.compute.amazonaws.com:4000/task/list")
+        .then((data) => data.json())
+        .then((data)=>{
+          console.log(data);
+          setTask(data);
+        });
+      },[]);
+    return (
+              
         <div>
+          
             <h1>Page4</h1>
+            {/* <Table columns={columns} dataSource={data} />; */}
+            <Table columns={columns} dataSource={task} onChange={onChange} />;
+            {/* <img src={image} />
             <p> There is Page4 </p>
             <a href='https://www.google.com/search?q=%E5%90%89%E5%A8%83%E5%A8%83&sxsrf=APwXEdcpm11YHuMFUZ8DhpQ7PamQ2tMQ6w:1682919587203&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjT67iBtNP-AhXwQPUHHcgtBwUQ_AUoAXoECAEQAw&biw=1536&bih=666&dpr=1.25#imgrc=e2F-FdB5yBEcHM'>看一下吉娃娃</a>
             <br></br>
-            <img src={doge} />
+            <img src={doge} /> */}
         </div>
     );
 }
--- src/components/Visuallizations.js
+++ src/components/Visuallizations.js
@@ -1,13 +1,34 @@
 import React from "react";
 import amazon from "../images/amazon.jpg";
+import { useState,useEffect } from "react";
+
+const data=fetch('https://dog.ceo/api/breeds/image/random',{
+    method:"GET"
+})
+    .then((res) => res.json())
+    .catch((e)=>{
+    console.log('api 讀取有誤');
+});
 
 function Visuallizations(){
+    const [image,setTmage]=useState('');
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((data)=>data.json())
+            .then((data)=>{
+                console.log(data.message);
+                setTmage(data.message);
+            });
+
+    },[]);
+
     return (        
         <div className="visuallizations">
             <h1>Visuallizations</h1>
             <p> There is Visuallizations </p>
             <a href='https://www.amazon.com'>前往Amazon</a>
             <br></br>
+            <img src={amazon} />
             <img src={amazon} />
 
             
